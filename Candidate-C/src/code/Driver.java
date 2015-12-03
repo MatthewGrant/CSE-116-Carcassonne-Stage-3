7a49632df6a25b8b32f419d764df2754a48853f3
Matthew Grant <mggrant@buffalo.edu>
2015-12-03 13:51:49 -0500

    added players

--- Candidate-C/src/code/Driver.java
+++ Candidate-C/src/code/Driver.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  */
 public class Driver {
 
+	
 	/**
 	 * The main method allows the game code to be run as a Java Application. Holds a for loop which places the player names into the ArrayList,String> named players. Also
 	 * creates the Board for actual gameplay.
@@ -16,14 +17,28 @@ public class Driver {
 	 * @param args	The player names listed in the command line input
 	 */
 	public static void main(String[] args){
-		ArrayList<String> players = new ArrayList<String>();
-		for (int i=0; i<args.length; i++) {
-			players.add(args[i]);
+		ArrayList<Player> players = new ArrayList<Player>();
+		if(args.length == 1){
+		
+			// load in Players here
+
+			
+			
+			PlayerTurns pt = new PlayerTurns(players);
+			pt.run();
+			
 		}
-		PlayerTurns pt = new PlayerTurns(players);
-		pt.run();
-		
-		
+		else{
+			for (int i=0; i<args.length; i++) {
+				String playerName = args[i];
+				Player player  = new Player(playerName);
+				players.add(player);
+			}
+			
+			PlayerTurns pt = new PlayerTurns(players);
+			pt.run();
+			
+		}
 		
 	}
 }
